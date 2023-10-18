d41574585953afd56636481876747e921965bc34
Noam Zeise <noam4134@gmail.com>
2023-10-18 08:41:50 +0100

    fixed bugs with renderbuffer creation and deletion

--- src/image.lisp
+++ src/image.lisp
@@ -3,9 +3,8 @@
 
 (deftype image-format () '(member :red :rg :rgb :rgba :depth24-stencil8))
 
+
 ;;; ----------  Texture -------------
-
-
 
 (defclass texture (gl-object)
   ((tex-type :initarg :tex-type :accessor tex-type :type texture-type)))
@@ -23,7 +22,8 @@
 	   (texture-wrap wrap) (texture-filter filter))
   "Make a texture, will be a multisample image if samples > 1.
 Data is a pointer to unsigned bytes or unsigned byte array."
-  (assert (and (> 0 width) (> 0 height) (> 0 samples)))
+  (assert (and (> width 0) (> height 0) (> samples 0)) (width height samples)
+	  "Width (~d) Height (~d) Samples (~d) must all be greater than 0" width height samples)
   (let ((id (gl:gen-texture))
 	(type (if (> samples 1) :texture-2d-multisample :texture-2d)))
     (gl:bind-texture type id)
@@ -37,7 +37,7 @@ Data is a pointer to unsigned bytes or unsigned byte array."
 	       (gl:tex-parameter type :texture-min-filter filter)
 	       (gl:tex-parameter type :texture-mag-filter filter)))
     (gl:bind-texture type 0)
-    (make-instance 'texture :id id :type type)))
+    (make-instance 'texture :id id :tex-type type)))
 
 (defmethod delete-gl ((obj texture))
   (gl:delete-texture (id obj)))
@@ -50,14 +50,14 @@ Data is a pointer to unsigned bytes or unsigned byte array."
 (defun make-renderbuffer (format width height samples)
   (declare (image-format format) (integer width) (integer height) (integer samples))
   "Make a renderbuffer, multisampled if samples > 1"
-  (assert (and (> 0 width) (> 0 height) (> 0 samples)))
+  (assert (and (> width 0) (> height 0) (> samples 0)))
   (let ((id (gl:gen-renderbuffer)))
-    (gl:bind-renderbuffer id)
+    (gl:bind-renderbuffer :renderbuffer id)
     (if (> 1 samples)
-	(gl:renderbuffer-storage-multisample id samples format width height)
-	(gl:renderbuffer-storage id format width height))
-    (gl:bind-renderbuffer 0)
+	(gl:renderbuffer-storage-multisample :renderbuffer samples format width height)
+	(gl:renderbuffer-storage :renderbuffer format width height))
+    (gl:bind-renderbuffer :renderbuffer 0)
     (make-instance 'renderbuffer :id id)))
 
 (defmethod delete-gl ((obj renderbuffer))
-  (gl:delete-framebuffers (id obj)))
+  (gl:delete-framebuffers (list (id obj))))
--- test.lisp
+++ test.lisp
@@ -15,6 +15,7 @@
 (with-game (:title "Test")
   (delete-gl (make-shader #p"shaders/vert.vs" #p"shaders/frag.fs"))
   (delete-gl (make-texture :rgba 100 100))
+  (delete-gl (make-renderbuffer :rgb 100 100 1))
   (loop until (gficl::game-closed-p)
 	do (test-render)
 	do (test-update))
