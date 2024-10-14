595513fb072d678e2aa1883506aa1759c5bf26b9
Noam Zeise <noam4134@gmail.com>
2024-10-14 13:38:35 +0100

    load/image returns the texture's width and height

--- src/load/image.lisp
+++ src/load/image.lisp
@@ -2,7 +2,8 @@
 
 (defun image (path &rest texture-key-args)
   "Load a png file into a texture from given path. 
-TEXTURE-KEY-ARGS holds any keyword args to GFICL:MAKE-TEXTURE except :format and :data"
+TEXTURE-KEY-ARGS holds any keyword args to GFICL:MAKE-TEXTURE except :format and :data.
+Returns as values a GFICL:TEXTURE, texture width, and texture height"
   (pngload:with-png-in-static-vector (png path)
     (let* ((width (pngload:width png))
 	   (height (pngload:height png))
@@ -10,8 +11,9 @@ TEXTURE-KEY-ARGS holds any keyword args to GFICL:MAKE-TEXTURE except :format and
 	   (bits (pngload:bit-depth png))
 	   (channels (/ (* len 8) (* width height bits))))
       (cffi:with-pointer-to-vector-data (data (pngload:data png))
-        (apply #'gficl:make-texture
-	       width height
-	       :format (gficl:get-image-format channels)
-	       :data data
-	       texture-key-args)))))
+        (values  (apply #'gficl:make-texture
+			width height
+			:format (gficl:get-image-format channels)
+			:data data
+			texture-key-args)
+		 width height)))))
