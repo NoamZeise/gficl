105286dbd62a2b944a3cb7e3604e413c8781c2ff
Noam Zeise <noam4134@gmail.com>
2024-11-13 00:23:13 +0000

    model also returns some texture map paths

--- src/load/model.lisp
+++ src/load/model.lisp
@@ -4,13 +4,32 @@
 
 (declaim (ftype (function ((or pathname string) &key (:vertex-form list)) list)
 		model))
-(defun model (model-path &key (vertex-form '(:position :normal :uv)))
+(defun model (model-path &key
+			 (vertex-form '(:position :normal :uv)))
   "takes a path to a model and returns a list of meshes (each mesh a VERTEX-DATA instance).
 :vertex-form is a list of VERTEX-ELEMENT (ie :position, :normal, :uv, or :skip)"
-  (loop for mesh in (obj:extract-meshes (obj:parse (probe-file model-path))) collecting
-	(gficl:make-vertex-data-from-vectors
-	 (get-vertex-form mesh vertex-form)
-	 (obj:vertex-data mesh) (obj:index-data mesh))))
+  (let ((meshes) (textures))
+    (loop for mesh in (obj:extract-meshes (obj:parse (probe-file model-path))) do	  
+	  (progn
+	    (setf meshes (cons (gficl:make-vertex-data-from-vectors
+				(get-vertex-form mesh vertex-form)
+				(obj:vertex-data mesh) (obj:index-data mesh))
+			       meshes))
+	    (setf textures (cons (let ((mat (obj:material mesh)))
+				   (if (not mat)
+				       nil
+				     (remove-if
+				      #'not
+				      (list (path :diffuse (obj:diffuse-map mat))
+					    (path :bump (obj:bump-map mat))
+					    (path :metallic (obj:metallic-map mat))
+					    (path :normal (obj:normal-map mat))))))
+				 textures))))
+    (values meshes textures)))
+
+(defun path (key val)
+  "return pairlis cons, where val is an obj texture map"
+  (if val (cons key (obj:file val)) nil))
 
 (defun test ()
   (gltf:with-gltf (torus (probe-file #p"examples/assets/torus.glb"))
